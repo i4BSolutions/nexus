c60628685b213e6dfe6393361e6eb3ab6e5213df
Aung Kaung Myat <115795473+akm-coding@users.noreply.github.com>
2025-06-25 11:55:55 +0700

    chore: add seeder in seed.sql

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -22,3 +22,10 @@ values
   ('Electronics'),
   ('Home & Kitchen'),
   ('Stationery');
+
+--  Seed data for the product currency table in Supabase
+insert into public.product_currency (currency_code, currency_name, is_active)
+values
+  ('MMK', 'Myanmar Kyat', true),
+  ('USD', 'United States Dollar', true),
+  ('EUR', 'Euro', true);
