ed3abc00598f4b33f0d2384ae9833f9628b22215
yanminthwin-i4b <yanminthwin@i4bsolutions.com>
2025-07-05 19:28:56 +0700

    update: seeder

---
+++ supabase/seeds/01_product.sql
@@ -0,0 +1,1014 @@
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100001',
+        'Like Anything',
+        'Home',
+        325.86,
+        1,
+        46,
+        'Western political teach force direction.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100002',
+        'Born Table',
+        'Outdoor',
+        110.97,
+        17,
+        87,
+        'Scientist herself somebody itself shoulder lose enjoy.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100003',
+        'Discussion Decade',
+        'Outdoor',
+        197.29,
+        20,
+        112,
+        'Civil model suffer cultural traditional event.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100004',
+        'Position Feel',
+        'Stationery',
+        301.36,
+        2,
+        34,
+        NULL,
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100005',
+        'Back Different',
+        'Home',
+        170.86,
+        1,
+        93,
+        NULL,
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100006',
+        'Fly Letter',
+        'Outdoor',
+        323.59,
+        20,
+        77,
+        'Education hand themselves increase.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100007',
+        'Start Bank',
+        'Stationery',
+        56.25,
+        12,
+        50,
+        'Future down lay attention before next.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100008',
+        'Claim Popular',
+        'Electronics',
+        183.77,
+        8,
+        21,
+        'Treatment medical sort voice language.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100009',
+        'Just Present',
+        'Clothing',
+        121.88,
+        15,
+        38,
+        'Charge support there long their effect maintain.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100010',
+        'Magazine Risk',
+        'Outdoor',
+        437.71,
+        16,
+        111,
+        'Stage at stay laugh garden.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100011',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100012',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100013',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100014',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100015',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100016',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100017',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100018',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100019',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100020',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100021',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100022',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100023',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100024',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100025',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100026',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100027',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100028',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100029',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100030',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100031',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100032',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100033',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100034',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
+
+INSERT INTO
+    public.product (
+        sku,
+        name,
+        category,
+        unit_price,
+        min_stock,
+        stock,
+        description,
+        currency_code_id,
+        is_active,
+        created_at,
+        updated_at
+    )
+VALUES
+    (
+        'AA-100035',
+        'Himself Design',
+        'Electronics',
+        90.24,
+        1,
+        60,
+        'Himself design its.',
+        1,
+        TRUE,
+        '2025-07-03T12:48:45.814858',
+        '2025-07-03T12:48:45.814858'
+    );
