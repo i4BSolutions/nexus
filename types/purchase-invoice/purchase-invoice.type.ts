e0abb8c889ce3c2f5eff4cf3e51e1f4d10d3bef2
thumin-dev <thumin@i4bsolutions.com>
2025-07-11 13:13:44 +0700

    feat: pi get all api

--- types/purchase-invoice/purchase-invoice.type.ts
+++ types/purchase-invoice/purchase-invoice.type.ts
@@ -12,3 +12,29 @@ export interface PurchaseInvoiceInterface {
   note?: string;
   invoice_items: PurchaseInvoiceItemInterface[];
 }
+
+export interface PurchaseInvoiceDto {
+  id: number;
+  purchase_invoice_number: string;
+  purchase_order_number: string;
+  invoice_date: string;
+  due_date: string;
+  currency_code: string;
+  usd_exchange_rate: number;
+  status: string;
+  note?: string;
+}
+
+export interface PurchaseInvoiceResponse {
+  items: PurchaseInvoiceDto[];
+  total: number;
+  page: number;
+  pageSize: number;
+  statistics: {
+    total: number;
+    total_invoiced: number;
+    total_allocated: number;
+    invoiced_percentage: number;
+    allocated_percentage: number;
+  };
+}
