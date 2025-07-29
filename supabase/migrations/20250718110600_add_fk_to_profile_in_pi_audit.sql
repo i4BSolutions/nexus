57baf336386d0559fbc7e6e6cff6c11c22ce2f5a
thumin-dev <thumin@i4bsolutions.com>
2025-07-29 12:41:51 +0700

    chore: change profile table name

--- supabase/migrations/20250718110600_add_fk_to_profile_in_pi_audit.sql
+++ supabase/migrations/20250718110600_add_fk_to_profile_in_pi_audit.sql
@@ -1,4 +1,4 @@
-alter table "public"."pruchase_invoice_audit_log" add constraint "pruchase_invoice_audit_log_changed_by_fkey" FOREIGN KEY (changed_by) REFERENCES profiles(id) not valid;
+alter table "public"."pruchase_invoice_audit_log" add constraint "pruchase_invoice_audit_log_changed_by_fkey" FOREIGN KEY (changed_by) REFERENCES user_profiles(id) not valid;
 
 alter table "public"."pruchase_invoice_audit_log" validate constraint "pruchase_invoice_audit_log_changed_by_fkey";
 
